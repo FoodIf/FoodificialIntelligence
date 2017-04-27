7ec7e5a3a119a0f5af5674abb155521c0fdf490e
hanal811 <hanal811@student.liu.se>
2017-04-27 13:02:33 +0200

    Mainactivity

---
+++ app/src/main/java/com/example/hannes/foodificialintelligence/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.hannes.foodificialintelligence;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Hannes on 2017-04-27.
+ */
+
+public class MainActivity extends Activity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainactivity);
+
+
+        };
+}
