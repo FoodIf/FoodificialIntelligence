2704d60b7b248d16fa56cadf0722b7835993ea63
hanal811 <hanal811@student.liu.se>
2017-05-02 16:12:22 +0200

    storemap

--- app/src/main/java/GUI/StoreActivity.java
+++ app/src/main/java/GUI/StoreActivity.java
@@ -1,7 +1,9 @@
 package GUI;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,27 +20,42 @@ import domain.Store;
  * Created by Hannes on 2017-05-02.
  */
 
-public class StoreActivity extends Activity {
+public class StoreActivity extends FragmentActivity implements OnMapReadyCallback {
     private Store store;
-
-    public StoreActivity(Store store) {
+    private GoogleMap mMap;
+   /* public StoreActivity(Store store) {
         this.store=store;
-    }
+    }*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView nameView = (TextView) findViewById(R.id.store_name);
+        setContentView(R.layout.activity_store);
+        /*TextView nameView = (TextView) findViewById(R.id.store_name);
         nameView.setText("Implementeramedfacadestore.getname()");
         TextView addressView = (TextView) findViewById(R.id.store_name);
         addressView.setText("Implementeramedfacadestore.getaddress()");
+        */
         //TODO ImplementeraAvstånd
         //TODO ImplementeraKarta
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        setContentView(R.layout.activity_store);
 
 
-    };
+
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
 
 }
