9bca713bc84a409accc6769ec4d9e1ac639acc35
hanal811 <hanal811@student.liu.se>
2017-05-02 13:12:41 +0200

    loginxml

---
+++ app/src/main/java/GUI/StoreActivity.java
@@ -0,0 +1,21 @@
+package GUI;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.hannes.foodificialintelligence.R;
+
+/**
+ * Created by Hannes on 2017-05-02.
+ */
+
+public class StoreActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+
+
+    };
+
+}
