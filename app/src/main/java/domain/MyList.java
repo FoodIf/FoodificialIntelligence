619f48f7d08ee09a6817ff92f4b2c2d88da805e4
Alexander Nilsson <alexander88.nilsson13@gmail.com>
2017-05-02 11:45:19 +0200

    Variabel och getters/ setters i MyList

--- app/src/main/java/domain/MyList.java
+++ app/src/main/java/domain/MyList.java
@@ -1,9 +1,24 @@
 package domain;
 
 
+import java.util.ArrayList;
+
 /**
  * Created by Alexander Nilsson on 2017-04-27.
  */
 
-public class MyList extends GroceryList {
+public class MyList {
+    private ArrayList <String> products;
+    public MyList(ArrayList<String> products) {
+        this.products = products;
+    }
+
+    public ArrayList<String> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<String> products) {
+        this.products = products;
+    }
 }
+
