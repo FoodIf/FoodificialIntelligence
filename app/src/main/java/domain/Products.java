447b22177bca9c388b1a679d1b128049dd15e5ff
albin_000 <albin.hjorth@gmail.com>
2017-05-02 13:29:11 +0200

    productList i databas skapad, Klassen Products utbyggd med Filereader o dää.

--- app/src/main/java/domain/Products.java
+++ app/src/main/java/domain/Products.java
@@ -1,21 +1,47 @@
 package domain;
 
 import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by albin_000 on 2017-05-02.
  */
 
 public class Products {
-    public void readProductList{
+
+    private ArrayList<String> productList;
+    private int rowCounter;
+
+    public void readProductList(){
+        if(productList == null) {
+            productList = new ArrayList();
+            setProductList(productList);
+        }
+        else{
+            clearProductList(productList);
+        }
+    }
+
+    public void setProductList(ArrayList<String> productList){
         try {
-            FileReader file = new FileReader("productlist.txt");
-        } catch(IOException e){
+            BufferedReader in = new BufferedReader(new FileReader("productList.txt"));
+            String productRow;
+            while ((productRow = in.readLine()) != null) {
+                productList.add(productRow);
+                rowCounter += 1;
+                for (int i = 0; i < productList.size(); i++) {
+                    System.out.print(productList.get(i));
+                }
+            }
+            in.close();
+        } catch (IOException e) {
             e.getMessage();
         }
-        while(file.hasNextLine()){
-
-        }
+    }
+    public ArrayList<String> clearProductList(ArrayList<String> productList){
+        productList.clear();
+        return productList;
     }
 }
