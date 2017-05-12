6dbff01b8a60dc0bf57a93865506f48b51d61414
albin_000 <albin.hjorth@gmail.com>
2017-05-12 10:02:50 +0200

    Databas-arrayer klara

---
+++ app/src/main/java/data/ProductList.java
@@ -0,0 +1,35 @@
+package data;
+
+import java.util.ArrayList;
+
+/**
+ * Created by albin_000 on 2017-05-12.
+ */
+
+public class ProductList {
+    public ProductList(){
+        ArrayList<String> productList = new ArrayList<>();
+        productList.add("Ketchup Heinz");
+        productList.add("Ketchup Felix");
+        productList.add("Tomater");
+        productList.add("Tomatpuré");
+        productList.add("Mellanmjölk");
+        productList.add("Smör");
+        productList.add("Kycklingfilé");
+        productList.add("Pyttipanna");
+        productList.add("Blåbär");
+        productList.add("Kalles Kaviar");
+        productList.add("Snickers");
+        productList.add("Ost");
+        productList.add("Filmjölk");
+        productList.add("Lax");
+        productList.add("Bröd");
+        productList.add("Falukorv");
+        productList.add("Gifflar");
+        productList.add("Bearnaise");
+        productList.add("Toalettpapper");
+        productList.add("Chips");
+        productList.add("Grill");
+        productList.add("Bregott");
+    }
+}
---
+++ app/src/main/java/data/UserList.java
@@ -0,0 +1,20 @@
+package data;
+
+import java.util.ArrayList;
+
+/**
+ * Created by albin_000 on 2017-05-12.
+ */
+
+public class UserList {
+    public UserList(){
+        ArrayList<String> userList = new ArrayList<>();
+        userList.add("hasselundmark@hot.com|hassekingen");
+        userList.add("fiskfittan300@gmail.com|sloth");
+        userList.add("growafro@hotdude.se|failarmy");
+        userList.add("poogboom@hotmale.com|sopa");
+        userList.add("spelarallan@hotmale.se|allanballan");
+        userList.add("zorroivan@gmail.com|ivanforpresident");
+        userList.add("test@test.com|test");
+    }
+}
