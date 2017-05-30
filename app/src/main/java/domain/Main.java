be8e3aae7f1eebd30b6d6e3d7bba516ca5b44bb9
Johan <johsk574@student.liu.se>
2017-05-30 13:04:13 +0200

    Uppdaterat produktlistorna.

--- app/src/main/java/domain/Main.java
+++ app/src/main/java/domain/Main.java
@@ -138,48 +138,6 @@ public class Main {
         comparedList.add(stringSum + " kr");
         return comparedList;
     }
-
-    /**
-     *
-     * @param //usedList
-     * @param //nearBy
-     * @return Ger tillbaka en hashmap med Store name som key och en "Arraylist<String>" där summan av alla varoar är utskrivan på sista raden i Arrayn.
-     */
-    /*public static HashMap<String, ArrayList<String>> storesToComp(MyList usedList, ArrayList<Store> nearBy){
-        ArrayList<String> newList;
-        HashMap<String, ArrayList<String>> sortedStores = new HashMap<String, ArrayList<String>>();
-        for (Store list:nearBy){
-            newList=cheapestList(usedList,list.getProductList());
-            int size = newList.size();
-            double sum = Double.parseDouble(newList.get(size-1));
-            sortedStores.put(list.getStoreName(),newList);
-        }
-        return sortedStores;
-    }
-    private static ArrayList<String> cheapestList(MyList myList, ArrayList<String> compWith){
-        String[] combo;
-        double sum=0;
-        ArrayList <String> productNsum = new ArrayList<>();
-        for (String product: myList.getProducts()) {
-            combo= compare(product, compWith);
-            sum =+ Double.parseDouble(combo[1]);
-            productNsum.add(combo[0]);
-        }
-        productNsum.add(String.valueOf(sum));
-        return productNsum;
-    }
-
-    private static String[] compare(String product, ArrayList<String> storeList) {
-        double price = 0;
-        String[] combo = new String[2];
-        for (String store : storeList) {
-            if (store.equals(product)) {
-                combo[1] = store.substring(store.indexOf("|"));
-            }
-            combo[0] = store;
-        }
-        return combo;
-    }*/
     public double distance(LatLng latlnguser, LatLng latlngstore) {
         int sizeofearth = 6371;
         double lat1 = latlnguser.latitude;
